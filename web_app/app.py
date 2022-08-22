95c3161c9fbdf1935e32b3a25f7e1e039fe0a94b
Matt Holubinka <meholubinka@gmail.com>
2022-08-22 17:29:58 +0100

    create basic geolocation app and dockerise it

---
+++ web_app/app.py
@@ -0,0 +1,18 @@
+import json
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def find_location():
+    uri = 'http://api.ipstack.com/check?access_key='
+    key = '35c43096adc9416dab6bdd2d1ad53069'
+    geolocationUri = uri + key
+    response = requests.get(geolocationUri)
+    response_json = json.loads(response.text)
+    lat = response_json['latitude']
+    long = response_json['longitude']
+    city = response_json['city']
+    txt = 'Hello Mudano! This application is running in {city}'
+    return txt.format(city = city)
---
+++ web_app/shart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting Flask web server..."
+uwsgi --ini /etc/uwsgi.ini
